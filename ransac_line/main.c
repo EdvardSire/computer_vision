88fb35195a52f65d5eab91b5ed606fc54080cdca
Edvard Sire <mail@edvardsire.com>
2025-01-29 03:36:02 +0100

    more

--- ransac_line/main.c
+++ ransac_line/main.c
@@ -1,20 +1,14 @@
 #include <SDL2/SDL.h>
 
-void drawPoint(SDL_Renderer *r, int x, int y) {
-  int width = 5;
+void drawRect(SDL_Renderer *r, int i) {
   SDL_SetRenderDrawColor(r, 255, 0, 0, 255);
-
+  SDL_Rect shape = {i, i, i+20, i+20};
+  SDL_RenderDrawRect(r, &shape);
 }
-void updateWindow(SDL_Renderer *r) {
-    SDL_Rect rect = {0, 0, 100, 100}; 
-    SDL_SetRenderDrawColor(r, 255, 255, 255, 255);
-
-    SDL_RenderClear(r); // Clear the scree
-    SDL_RenderFillRect(r, &rect); // Draw the rectangle
-                                  //
-    SDL_SetRenderDrawColor(r, 255, 0, 0, 255);
-    SDL_RenderFillRect(r, &point); // Draw the rectangle
-    SDL_RenderPresent(r); // Update the screen
+void drawBackground(SDL_Renderer *r, int height, int width) {
+  SDL_SetRenderDrawColor(r, 0, 0, 0, 255);
+  SDL_RenderDrawLine(r, 0, 500, 1000, 500);
+  SDL_RenderDrawLine(r, 500, 0, 500, 1000);
 }
 
 int main() {
@@ -22,20 +16,31 @@ int main() {
     SDL_Window *w = SDL_CreateWindow("Rect", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
     SDL_Renderer *r = SDL_CreateRenderer(w, -1, 0);
 
+
     int running = 1;
+    int i = 200;
     SDL_Event e;
 
     while (running) {
         while (SDL_PollEvent(&e)) {
           if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && (e.key.keysym.sym == SDLK_ESCAPE || e.key.keysym.sym == SDLK_q)))
                 running = 0;
+          if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_f)
+              i += 10;
+
+          if(e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_g)
+              i -= 10;
         }
+        SDL_SetRenderDrawColor(r, 255, 255, 255, 255);
+        SDL_RenderClear(r);
 
-        updateWindow(r);
+        drawBackground(r, 1000, 100);
+        drawRect(r, i);
 
-        SDL_Delay(10);
+
+        SDL_RenderPresent(r);
+        SDL_Delay(16);
     }
-
     SDL_DestroyRenderer(r);
     SDL_DestroyWindow(w);
     SDL_Quit();
