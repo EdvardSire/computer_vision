f5b19548a57b6da69ebc9da423d99e79eee4a8f8
Edvard Sire <mail@edvardsire.com>
2025-02-03 23:46:11 +0100

    draw sampled dots on screen

--- ransac_line/main.c
+++ ransac_line/main.c
@@ -2,26 +2,39 @@
 #include <SDL2/SDL.h>
 
 
+#define N_POINTS 200
+#define WIN_X 1000
+#define WIN_Y 1000
+
 typedef struct {
   int x;
   int y;
 } Point;
 
-void drawRect(SDL_Renderer *r, int x, int y, int h, int w) {
-  SDL_Rect rect = {x, y, w, h};
+void drawRect(SDL_Renderer *r, Point point) {
+  int size = 3;
+  SDL_Rect rect = {point.x + WIN_X/2, point.y + WIN_Y/2, size, size};
   SDL_SetRenderDrawColor(r, 255, 0, 0, 255);
   SDL_RenderDrawRect(r, &rect);
 }
 
 void drawBackground(SDL_Renderer *r, int height, int width) {
   SDL_SetRenderDrawColor(r, 0, 0, 0, 255);
-  SDL_RenderDrawLine(r, 0, 500, 1000, 500);
-  SDL_RenderDrawLine(r, 500, 0, 500, 1000);
+  SDL_RenderDrawLine(r, 0, WIN_Y/2, WIN_X, WIN_Y/2);
+  SDL_RenderDrawLine(r, WIN_X/2, 0, WIN_X/2, WIN_Y);
 }
+
+void randomPoints(Point points[N_POINTS]) {
+  for(int i = 0; i < N_POINTS; i++) {
+    points[i].x = (rand() % WIN_X) - WIN_X/2;
+    points[i].y = (rand() % WIN_Y) - WIN_Y/2;
+  }
+}
+
 
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
-    SDL_Window *w = SDL_CreateWindow("Rect", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 1000, 0);
+    SDL_Window *w = SDL_CreateWindow("Rect", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIN_X, WIN_Y, 0);
     SDL_Renderer *r = SDL_CreateRenderer(w, -1, 0);
 
 
@@ -29,7 +42,10 @@ int main() {
     int l = 10;
     SDL_Event e;
 
-    rand();
+    
+    Point points[N_POINTS];
+    randomPoints(points);
+
     while (running) {
         while (SDL_PollEvent(&e)) {
           if (e.type == SDL_QUIT || (e.type == SDL_KEYDOWN && (e.key.keysym.sym == SDLK_ESCAPE || e.key.keysym.sym == SDLK_q)))
@@ -45,8 +61,9 @@ int main() {
 
         drawBackground(r, 1000, 100);
 
-        for(int i = 0; i < l; i++) {
-          drawRect(r, i*20, i*20, 20, 20);
+
+        for(int i = 0; i < N_POINTS; i++) {
+          drawRect(r, points[i]);
         }
 
 
